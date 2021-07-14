ed9e34548ca23d570a79a7c611c6e467c4347988
Vishal Das <vishaldas521@gmail.com>
2021-07-14 18:39:18 +0530

    Corrected variable name

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const http = require('http').createServer(app)
-const port = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 
 //here the port is setup to run the app
